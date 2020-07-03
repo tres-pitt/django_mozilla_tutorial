3ef245ce6cd6e9ebb6e5e73e1cb489d9fe1c2397
Tres Pittman <trespittman@pm.me>
2020-07-03 13:58:01 -0600

    admin site including new books

--- catalog/models.py
+++ catalog/models.py
@@ -44,6 +44,12 @@ class Book(models.Model):
     # ManyToManyField used because genre can contain many books. Books can cover many genres.
     # Genre class has already been defined so we can specify the object above.
     genre = models.ManyToManyField(Genre, help_text='Select a genre for this book')
+
+    def display_genre(self):
+        """Create a string for the Genre. This is required to display genre in Admin."""
+        return ', '.join(genre.name for genre in self.genre.all()[:3])
+
+    display_genre.short_description = 'Genre'
     
     def __str__(self):
         """String for representing the Model object."""
